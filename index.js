932f322b2018186fae8360666a095186812d50cf
Andraelja <eljapramaandra@gmail.com>
2025-09-24 15:42:19 +0700

    roleback

--- index.js
+++ index.js
@@ -1,41 +1,25 @@
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const router = require('./routes');
-// const fileUpload = require('express-fileupload');
-
-// const app = express();
-
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(fileUpload());
-// app.use(express.static("public"));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// app.use('/api', router);
-
-// module.exports = app;
-const express = require('express');
+const express = require('express')
 const cors = require('cors');
-const router = require('./routes');
+const bodyParser = require('body-parser');
+const router = require('./routes')
+const fileUpload = require('express-fileupload')
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(fileUpload());
+app.use(express.static("public"))
 
-// route default
+const port = 3000;
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!')
 });
 
-// router API
 app.use('/api', router);
 
-// export app untuk Vercel
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
